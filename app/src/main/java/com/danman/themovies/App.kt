05e9b1ee4b53851e6e9c7a1027e1bb4ea3e96459
danil <5699320@gmail.com>
2019-11-09 16:36:58 +0200

    initial

---
+++ app/src/main/java/com/danman/themovies/App.kt
@@ -0,0 +1,12 @@
+package com.danman.themovies
+
+import androidx.multidex.MultiDexApplication
+
+/**
+ * Created with Android Studio.
+ * User: Danil Konovalenko
+ * Date: 2019-11-09
+ * Time: 15:24
+ */
+class App : MultiDexApplication() {
+}
---
+++ app/src/main/java/com/danman/themovies/presentation/feature/splash/SplashActivity.kt
@@ -0,0 +1,10 @@
+package com.danman.themovies.presentation.feature.splash
+
+/**
+ * Created with Android Studio.
+ * User: Danil Konovalenko
+ * Date: 2019-11-09
+ * Time: 14:15
+ */
+class SplashActivity {
+}
